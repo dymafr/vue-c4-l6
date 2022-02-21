847e10fc47073b01c387eafdbcb724fa9363b1a9
dymafr <erwan1309@hotmail.com>
2022-02-21 17:08:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-rqewil
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-rqewil)
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>Prix total HT : {{ totalPrixHT }}€</h2>
+  <h2>Prix total TTC : {{ totalPrixTTC }}€</h2>
+</template>
+
+<script setup lang="ts">
+import { computed, reactive } from 'vue';
+
+const produit = reactive({
+  quantite: 3,
+  prix: 10,
+  nom: 'livre',
+});
+
+const totalPrixHT = computed(() => produit.quantite * produit.prix);
+const totalPrixTTC = computed(() => produit.quantite * produit.prix * 1.2);
+</script>
+
+<style></style>
